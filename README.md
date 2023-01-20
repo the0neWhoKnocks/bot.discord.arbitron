125adf00f87f22513ff71176d268d41d924e3c41
Nox <the0neWhoKnocks@users.noreply.github.com>
2023-01-19 19:16:09 -0800

    Added boilerplate
    Added 2-way communication with a test Discord Server.

--- README.md
+++ README.md
@@ -1,7 +1,79 @@
 # Arbitron
 
-A Discord bot that'll pick a random (arbitrary) item from a specified list
+A Discord bot that'll pick a random (arbitrary) item from a specified list.
+
+- [Development](#development)
+  - [Discord](#discord)
+  - [NodeJS](#nodejs)
+- [Sources](#sources)
 
 ---
 
+## Development
 
+### Discord
+
+1. Create a local `.env` file within this cloned repo.
+1. In Discord go to your profile's `User Settings`
+   - Go to `Advanced`
+      - Enable `Developer Mode`
+1. Create a test Server for you to mess around in.
+1. Right-click on the test Server, click on `Copy ID`. The Server ID is also referred to as `Guild ID`.
+   - Add this to your `.env` file:
+     ```
+     SERVER_ID=<ID>
+     ```
+1. Go to https://discord.com/developers/applications to start creating the application.
+   - Click `New Application`
+      - `Name: Arbitron` (click `Create`)
+   - In the left menu:
+      - General Information
+         - Add a description
+         - Copy the `APPLICATION ID` and `PUBLIC KEY`. Discord sometimes refers to `APPLICATION ID` as `CLIENT ID`.
+         - Add those values to your `.env` file:
+            ```
+            APPLICATION_ID=<ID>
+            PUBLIC_KEY=<KEY>
+            ```
+      - Bot
+         - Click `Add Bot`
+         - Click `Reset Token` to generate a token. Add the token to your `.env` file:
+            ```
+            BOT_TOKEN=<TOKEN>
+            ```
+      - OAuth2 > URL Generator
+         ```
+         [Scopes]
+           [X] bot
+           [X] applications.commands
+         
+         [Bot Permissions]
+           [General Permissions]
+             [X] Read Messages/View Channels
+           
+           [Text Permissions]
+             [X] Send Messages
+             [X] Send Messages in Threads
+             [X] Use Slash Commands
+         ```
+         - Copy the URL in the `GENERATED URL` section.
+1. Open the copied URL in another tab, and add the application to the test Server.
+
+
+### NodeJS
+
+1. Within this repo, run:
+   ```sh
+   npm i
+   ```
+1. Start up in Dev mode:
+   ```sh
+   npm run start:dev
+   ```
+1. If you see a message that reads something like `Logged in as Arbitron`, go to your Discord Server and type `/arb` to see if the commands pop up.
+
+---
+
+## Sources
+
+- https://discordjs.guide/preparations/
---
+++ src/commands/arb.js
@@ -0,0 +1,114 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+const addRemove = ({ name, description }) => (subcommand) => {
+  return subcommand
+    .setName(name)
+    .setDescription(description)
+    .addStringOption(option =>
+			option
+				.setName('list')
+				.setDescription(`The list name where a choice will be ${(name === 'add') ? 'added' : 'removed'}.`)
+				.setRequired(true))
+    .addStringOption(option =>
+			option
+				.setName('choices')
+				.setDescription('One or more comma separated items')
+				.setRequired(true));
+};
+
+const pick = (subcommand) => {
+  return subcommand
+    .setName('pick')
+    .setDescription('Pick a random choice from a given list.')
+    .addStringOption(option =>
+			option
+				.setName('list')
+				.setDescription('The list where a random choice will be picked.')
+				.setRequired(true));
+};
+
+const view = (subcommand) => {
+  return subcommand
+    .setName('view')
+    .setDescription('Displays all the items in a given list.')
+    .addStringOption(option =>
+			option
+				.setName('list')
+				.setDescription('The name of the list.')
+				.setRequired(true));
+};
+
+const data = new SlashCommandBuilder()
+  .setName('arb')
+  .setDescription('Pick a random choice from a list')
+  .addSubcommand( addRemove({ name: 'add', description: 'Add choice(s) to a list' }) )
+  .addSubcommand( addRemove({ name: 'remove', description: 'Remove choice(s) from a list' }) )
+  .addSubcommand(pick)
+  .addSubcommand(view);
+
+module.exports = {
+  data,
+  async execute(interaction) {
+    const subCommand = interaction.options.getSubcommand();
+    const msg = () => customMsg || `User requested: "${cmd}"`;
+    let ephemeral = false;
+    let cmd;
+    let customMsg;
+    
+    switch (subCommand) {
+      case 'add':
+      case 'remove': {
+        const list = (interaction.options.getString('list') ?? '').trim().toLowerCase().replace(/[\s_]/g, '-');
+        const choices = (interaction.options.getString('choices') ?? '').split(',').filter(item => !!item);
+        const serverId = interaction.member.id;
+        const userId = interaction.user.id;
+        const userName = interaction.user.username;
+        
+        if (!list) {
+          return await interaction.reply({ content: 'You need to provide a list', ephemeral: true });
+        }
+        else if (!choices.length) {
+          return await interaction.reply({ content: 'You need to provide at least one choice', ephemeral: true });
+        }
+        
+        // TODO:
+        // - if adding, create list under group by `serverId`
+        // - if removing, and no items remain, remove list/group by `serverId`
+        // - add/remove item(s)
+        
+        const color = (subCommand === 'add') ? '00FFAA' : 'FFB500';
+        const exampleEmbed = new EmbedBuilder()
+          .setColor(`0x${color}`)
+          .setTitle(`Updated "${list}" list`)
+          .setDescription([
+            `${userName} ${(subCommand === 'add') ? 'added' : 'removed'} ${(choices.length === 1) ? 'this item' : 'these items'}:`,
+            choices.map((c) => `"${c}"`).join(', '),
+          ].join(' '));
+        
+        return await interaction.reply({
+          embeds: [exampleEmbed],
+          ephemeral: false,
+        });
+      }
+        
+      case 'pick':
+        // TODO: 
+        // - get items from DB
+        // - generate random seed from `userId` and timestamp
+        // - pick item
+        return await interaction.reply({
+          content: 'List of items',
+          ephemeral: true,
+        });
+        
+      case 'view':
+        // TODO: get items from DB
+        return await interaction.reply({
+          content: 'List of items',
+          ephemeral: true,
+        });
+    }
+    
+    await interaction.reply({ content: msg(), ephemeral });
+  },
+};
